df5fa44d9da934f4edc0621e4547b8c46084b398
Kartik Pant <kartikpant.kp69@gmail.com>
2025-08-02 03:59:45 +0530

    three.js__ frontend

---
+++ Whatsapp/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { AuroraHero } from "./AuroraHero";
+
+function App() {
+  return (
+    <div>
+      <AuroraHero />
+      {/* other components */}
+    </div>
+  );
+}
+
+export default App;
